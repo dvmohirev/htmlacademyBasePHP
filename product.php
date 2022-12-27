110cccc257f0d3bf39902c4102215dbba90f9ab0
Дмитрий Мохирев <dvmohirev@gmail.com>
2022-12-27 15:46:43 +0500

    Задача 15. Замените значение переменной $id на $_GET['product_id'] В адресной строке поменяйте число 1 на 9 В мини-браузере перейдите на вкладку catalog.php Щёлкните на третий товар — «Блетуб». Убедитесь, что на странице товара отображается выбранный товар.

--- product.php
+++ product.php
@@ -7,15 +7,14 @@ require('products_db.php');
 require('components/header.php');
 
 // Объявите переменную здесь
-$id = 7;
+$id = $_GET['product_id'];
 $title = get_product_title($id);
 $img_url = get_img_url($id);
 
-$getParam = $_GET['product_id'];
 require('components/product_info.php');
 require('components/footer.php');
 ?>
 <div class="data-php" data-attr="<?=$title; ?>"></div>
-<div class="data-php1" data-attr="<?=$getParam; ?>"></div>
+<div class="data-php1" data-attr="<?=$id; ?>"></div>
 <script type="application/javascript" src="script.js"></script>
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var title = document.querySelector('.data-php').getAttribute('data-attr');
-var getParam = document.querySelector('.data-php1').getAttribute('data-attr');
+var id = document.querySelector('.data-php1').getAttribute('data-attr');
 console.log(title);
-console.log(getParam);
+console.log(id);
