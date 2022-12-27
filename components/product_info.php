50d1c6e50cd6a2891dc93ee8e0f20aee59140b18
Дмитрий Мохирев <dvmohirev@gmail.com>
2022-12-27 15:39:55 +0500

    Задача 12. После переменной $title создайте переменную $img_url Запишите в эту переменную get_img_url(10) Переключитесь в редакторе на вкладку c шаблоном components/product_info.php Добавьте переменную $img_url в атрибут src на 4 строке с помощью сокращённой записи: <?= $img_url ?> По аналогии добавьте переменную $title в атрибут alt

--- components/product_info.php
+++ components/product_info.php
@@ -1,6 +1,6 @@
 <section class="item">
     <div class="container">
         <h1 class="item-title"><?php echo($title)?></h1>
-        <img src="img/placeholder.png" width="510" height="392" alt="Заготовка для фото товара">
+        <img src="<?= $img_url ?>" width="510" height="392" alt="<?= $title ?>">
     </div>
 </section>
--- product.php
+++ product.php
@@ -7,6 +7,8 @@ require('products_db.php');
 require('components/header.php');
 
 $title = get_product_title(5);
+// Объявите переменную здесь
+$img_url = get_img_url(10);
 
 require('components/product_info.php');
 require('components/footer.php');
