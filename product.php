6181d8f8b46c8bab6a5a1caf1894a32f7b572c46
Дмитрий Мохирев <dvmohirev@gmail.com>
2022-12-27 15:45:18 +0500

    Задача 14. Не забывайте писать параметр в конце адресной строки - product_id=8 Выведите в консоль команду $_GET['product_id'] с помощью keks_log. В адресной строке поменяйте значение параметра product_id на 8.

--- product.php
+++ product.php
@@ -11,9 +11,11 @@ $id = 7;
 $title = get_product_title($id);
 $img_url = get_img_url($id);
 
+$getParam = $_GET['product_id'];
 require('components/product_info.php');
 require('components/footer.php');
 ?>
 <div class="data-php" data-attr="<?=$title; ?>"></div>
+<div class="data-php1" data-attr="<?=$getParam; ?>"></div>
 <script type="application/javascript" src="script.js"></script>
 
--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 var title = document.querySelector('.data-php').getAttribute('data-attr');
+var getParam = document.querySelector('.data-php1').getAttribute('data-attr');
 console.log(title);
+console.log(getParam);
